ade6aad0680035a27ea0cec0991486d542869767
claucanchin <claucanchin@gmail.com>
2019-03-24 19:18:18 +0800

    implemented main game logic

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Simon is named after the simple children's game of Simon Says. A round in the game consists of the buttons lighting up in a random order, after which the player must reproduce that order by clicking the buttons in the same sequence. As the game progresses, the number of buttons to be clicked increases. If the user succeeds, the series become progressively longer and more complex. 
 
-#### Build Simon (User Story)
+#### User Story
 As a user, 
 - I can see four buttons (clockwise: green, red, blue, yellow)
 - I can click on a start button that does a count down before the button lights up
@@ -16,42 +16,3 @@ As a user,
 - Add another color
 - Rotate the placement of the buttons
 - Use keyboard as input instead of clicking
-
-### Pseudo-code 
-This program will allow the user to click on 4 buttons. 
-Each click will be record down to an array and compare with the game result to check if user win or not after a number of clicks
-
-if user click on red button
-   add "red" to clickArray
-   change red button to gray for 1 seconds to let user know that they clicked the button
-   reset previous clicked button to normal state
-   update previous clicked button to red
-
-if user click on green button
-   add "green" to clickArray
-   change green button to gray for 1 seconds to let user know that they clicked the button
-   reset previous clicked button to normal state
-   update previous clicked button to green
-
-First round:
-levelCount is 1
-Simon will look at all buttons (buttonsArray)
-Simon will random select x number of colors - levelCount
-Save (push) random colors into simonArray
-
-	repeatable: Simon will flash the buttons in simonArray
-		run detectWinState
-
-Win state:
-If the user's clickArray matches simonArray
-	add levelCount
-	add one random color to simonArray
-	clear clickArray
-	show some message
-	repeat flash buttons (next round)
-Else 
-	clear clickArray
-	show some message
-	repeat simonArray
-
-Reset: clear simonArray and clickArray
--- script.js
+++ script.js
@@ -3,8 +3,9 @@ console.log("hello simon");
 //find location of the buttons and put into an array to manipulate later
 var buttonsClass = document.querySelectorAll(".buttons")[0];
 var buttonsArr = [];
-var levelCount = 1;
 var simonArray = [];
+var playerArray =[];
+var clickCount = 0;
 
 for (var i = 0; i < buttonsClass.children.length; i++) {
     buttonsArr.push(buttonsClass.children[i]);
@@ -34,12 +35,55 @@ var startButton = document.querySelector("#start");
 startButton.addEventListener("click", start);
 
 function start() {
-    for (var i = 0; i < levelCount; i++) {
-        simonArray.push(randomSelect(buttonsArr));
-    }
+    this.style.display = 'none';
+    newRound();
+}
+
+function newRound() {
+    simonArray.push(randomSelect(buttonsArr));
     flashButtons(simonArray);
 }
 
 var randomSelect = function(buttons)  {
     return buttons[Math.floor(Math.random()*buttonsArr.length)];
+}
+
+for (var i = 0; i < buttonsArr.length; i++) {
+    buttonsArr[i].addEventListener("click",
+        //put clicked buttons into an array
+        function(button) {
+          return function() {
+                playerArray.push(button);
+                clickCount += 1;
+                checkWin();
+            }
+        }(buttonsArr[i]));
+}
+
+//compare both arrays: return false as soon as length does not match, then return false as soon as index does not match
+var isSame = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
+//when player clicks, compare number of clicks with simon array, if matches then check isSame
+var checkWin = function() {
+    if (clickCount === simonArray.length) {
+        if (isSame(playerArray, simonArray)) {
+            console.log("It's a match");
+            playerArray = [];
+            clickCount = 0;
+            newRound();
+        } else {
+            console.log("You lose, try again");
+            playerArray = [];
+            clickCount = 0;
+            flashButtons(simonArray);
+        }
+    }
 }
